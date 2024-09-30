3c3f7f0ad1754e53b07a736665a9e48d4baa1954
Tasnim <tasnimalbrashdi@gmail.com>
2024-09-30 15:40:15 +0400

    Add strcuture program

--- Shape/Program.cs
+++ Shape/Program.cs
@@ -1,10 +1,24 @@
 ﻿namespace Shape
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
         }
     }
+    public class shape
+    {
+        protected double lentgh;
+        protected double width;
+
+        public virtual double area() { 
+            
+            return 0;
+        }
+    }
+    public class sequre :shape { }
+    public class circle :shape { }
+    public class tringle :shape { }
+    public class rectangle :shape { }
 }
